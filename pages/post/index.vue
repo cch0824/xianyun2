d2bc3544dbe8c71f99f3cb259cd80bd36a728a4b
JacKie <shaojiaqi08@163.com>
2019-08-09 19:22:53 +0800

    完成旅游攻略首页，剩下跳转页面未完成

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,11 +1,75 @@
 <template>
-    <div class="index">旅游攻略模块首页文件</div>
+  <!-- <p>文章首页</p> -->
+  <div class="post">
+    <el-row>
+      <el-col :span="6">
+        <postAside @tuijiancity="searchcity"></postAside>
+      </el-col>
+      <el-col :span="18">
+        <postmain :data="data" @city="searchcity" @pagenum='pagenum'></postmain>
+      </el-col>
+    </el-row>
+  </div>
 </template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="less" scoped>
 
+<script>
+import postAside from "@/components/post/postAside";
+import postmain from "@/components/post/postmain";
+export default {
+  data() {
+    return {
+      data: {
+        data: []
+      },
+      limit: 3,
+      start: 0
+    };
+  },
+  methods: {
+    searchcity(city) {
+      // console.log(this.start);
+      let params = {
+        _start: this.start, // 索引
+        _limit: this.limit // 数量
+      };
+
+      if (city) {
+        params.city = city;
+      }
+      this.$axios({
+        url: "/posts",
+        params
+      }).then(res => {
+        // console.log(res.data)
+        this.data = res.data;
+        this.data.data.forEach(item => {
+          if (item.images.length > 3) {
+            item.images.length = 3;
+          }
+        });
+        console.log(res.data);
+      });
+    },
+
+    pagenum(num,size){
+      this.start = (num-1)*size
+      this.limit = size
+      this.searchcity()
+    }
+  },
+  components: {
+    postAside,
+    postmain
+  },
+  mounted() {
+    this.searchcity();
+  }
+};
+</script>
+
+<style scoped lang='less'>
+.post {
+  width: 1000px;
+  margin: 0 auto;
+}
 </style>
